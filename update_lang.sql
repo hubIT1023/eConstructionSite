cc71766af8486a719cf65689a7737f9dbea34d51
plasma_spark <hubittoday@gmail.com>
2026-09-01 02:59:21 +1200

    Removed update_lang.sql after successful database execution

--- update_lang.sql
+++
@@ -1,5 +0,0 @@
-UPDATE tbl_language SET lang_value = 'Delivery Cost' WHERE lang_id = 84;
-UPDATE tbl_language SET lang_value = 'Update Delivery Address' WHERE lang_id = 87;
-UPDATE tbl_language SET lang_value = 'Update Billing and Delivery Info' WHERE lang_id = 88;
-UPDATE tbl_language SET lang_value = 'Billing and Delivery Information is updated successfully.' WHERE lang_id = 122;
-UPDATE tbl_language SET lang_value = 'Delivery Address' WHERE lang_id = 162;
